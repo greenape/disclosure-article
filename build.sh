38c7cef8b9ee819066e0e3cd66f591b93355e0e0
Jonathan Gray <j.gray@soton.ac.uk>
2014-08-07 22:05:21 +0100

    Figures & text
    Obviously

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+pdflatex -interaction=batchmode disclosure_game
+makeglossaries disclosure_game
+bibtex disclosure_game
+pdflatex -interaction=batchmode disclosure_game
+pdflatex -interaction=batchmode disclosure_game

75108043db8bb6d87ae8721582a06bc8072f1a4e
Jonathan Gray <j.gray@soton.ac.uk>
2014-08-30 16:46:06 +0100

    Bit more on results

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ makeglossaries disclosure_game
 bibtex disclosure_game
 pdflatex -interaction=batchmode disclosure_game
 pdflatex -interaction=batchmode disclosure_game
+pdflatex -interaction=batchmode disclosure_game

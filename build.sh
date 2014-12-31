584d7263e46f9f2e72f4ef19fdb1307f57d86ff6
Jonathan Gray <j.gray@soton.ac.uk>
2014-12-31 11:53:49 +0000

    More Springer jazz

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-pdflatex -interaction=batchmode disclosure_game
+rm disclosure_game.aux
+rm disclosure_game.bbl
+rm disclosure_game.blg
+rm disclosure_game.idx
+rm disclosure_game.log
+rm disclosure_game.out
+rm disclosure_game.pdf
+latex -interaction=batchmode disclosure_game
 makeglossaries disclosure_game
 bibtex disclosure_game
 pdflatex -interaction=batchmode disclosure_game
